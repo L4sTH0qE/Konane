47ecb62f8234e6da6a686b51092f120853ade53e
Alexander Solovkin <solovkin1990@gmail.com>
2024-04-01 16:41:35 +0300

    tweak(code): Minor fixes

--- Konane/ClientApp/src/components/Pages/Home.js
+++ Konane/ClientApp/src/components/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, {Component, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import useInterval from "../useInterval";
 
 export default function Home (props) {
